2dfb4ce1cb5d16daf3b913c341c64b02d4c54035
Arun raj <riderarunraj@live.com>
2025-01-06 12:08:06 +0530

    js

---
+++ HTML/JS/variable.js
@@ -0,0 +1,288 @@
+// var name = "arun";
+
+
+
+
+// console.log(name + "arkha");
+
+
+// let ar = 1234;
+
+// console.log(ar);
+// {
+//     let ar = "arun"
+//     console.log(ar);
+// }
+
+
+
+// let age = 25;
+// age = 26;
+
+// console.log(age);
+
+
+
+
+// const ar = "raj";
+
+// {
+//     const ar = "122342"
+    
+// }
+
+// console.log(ar);
+
+
+
+// let myVar = "ashf";
+
+// let myvar = "jydsgfdjh"
+
+// let city = "ne yoprk ";
+
+
+// let city ; 
+
+// city = "chennai"
+
+
+// let greet = "hello-100"
+
+// let scores = 100;
+
+
+
+// let arun = true;
+
+
+
+
+
+
+// let NotAssigned ;
+// console.log(NotAssigned);
+
+// NotAssigned = 123;
+// console.log(NotAssigned);
+
+
+// let Nully = null
+// console.log(Nully);
+
+// Nully = 123245; 
+
+
+
+// let Person = {
+//     name : "arun",
+//     age : 400,
+//     city: "Balli"
+// }
+
+// console.log(Person);
+
+// let color =["red", "blue", "green"]
+
+
+
+
+// let firstname  = "arun";
+// let lastname = "raj";
+// let age = 25;
+
+
+// console.log("my Namee is " + firstname +" "+ lastname+".");
+
+
+// age = age + 1;
+// console.log("Next year my age will be" + age +" Years Old" + lastname+".");
+
+
+// let a = 10 ;
+
+// let b = 3;
+
+
+// console.log(a+b);
+// console.log(a -b );
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a % b );
+
+
+// let x = 10 
+// x += 5
+// console.log(x);
+
+
+// x *= 2;
+
+
+// console.log(x);
+
+
+// == 5 == "5"
+// ===  
+// != 
+// !==
+// >
+// <
+
+// let num1 = 10
+// let num2 = "10";
+
+
+// console.log(num1 == 100);
+
+// && 
+// ! 
+
+// let isSunny = true ;
+// let hasumbrella = false ;
+
+
+// console.log(isSunny && hasumbrella);
+
+
+// console.log(isSunny || hasumbrella);
+
+// console.log(!isSunny);
+
+
+
+
+
+
+
+// let score = 50 ;
+// let maxScore = 100;
+
+// let isPassed = score >= 40;
+
+
+// console.log(score +10);
+// score += 20
+// console.log(score > maxScore);
+
+// console.log(isPassed && score >= 50);
+
+// if (condition ){
+//     //excicte the code 
+// }
+
+// let age = 18 ;
+
+// if (age >= 18){
+//     console.log("you are eligible to Vote ");
+// }
+
+
+
+// if (condit){
+// code 
+// }else{
+//     code 
+// } 
+
+
+// let temp = 25;
+
+// if (temp > 30){
+//     console.log("its hot houtside");
+// }else if ( conditon 2)  {
+//     console.log("the Weather is Plesant on this planet  ");
+// }else {
+
+// }
+
+// let marks = 85;
+
+// if (marks >= 90) {
+//   console.log("Grade: A");
+// } else if (marks >= 75) {
+//   console.log("Grade: B");
+// } else if (marks >= 50) {
+//   console.log("Grade: C");
+// } else {
+//   console.log("Grade: F");
+// }
+
+
+
+// switch (expression) {
+//     case value1:
+//       // Code to execute if expression === value1
+//       break;
+//     case value2:
+//       // Code to execute if expression === value2
+//       break;
+//     default:
+//       // Code to execute if none of the cases match
+//   }
+  
+
+// let day = 5;
+
+// switch (day) {
+//     case 1:
+//         console.log("monday");
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+//     case 3:
+//         console.log("wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+
+//     default :
+//         console.log("invalid day");
+    
+// }
+
+
+let time = 15;
+
+if (time < 12) {
+  console.log("Good Morning!");
+} else if (time < 18) {
+  console.log("Good Afternoon!");
+} else {
+  console.log("Good Evening!");
+}
+
+
+
+
+
+let color = "red";
+
+switch (color) {
+  case "red":
+    console.log("Stop!");
+    break;
+  case "yellow":
+    console.log("Get Ready!");
+    break;
+  case "green":
+    console.log("Go!");
+    break;
+  default:
+    console.log("Invalid color");
+}
+
+
+
+
+
+
+
+
+
+
