f276acd581b4fe7166b26b33dadaa5bed947bee6
Arun raj <riderarunraj@live.com>
2025-01-12 12:58:13 +0530

    s

---
+++ HTML/JS/Day2/script.js
@@ -0,0 +1,137 @@
+// for (intialiazion ; condion ; increse/decrease ){
+//     //code to be exicuted 
+// }
+
+
+// for (let i=0 ; i<5; i++){
+//     console.log(i);
+// }
+
+
+
+// const products = ["Laptop", "Mobile", "Tablet", "Headphones"];
+
+// console.log("Product List");
+
+// for(i=0; i < products.length; i++){
+//     console.log(`${i+1}. ${products[i]}`);
+// }
+
+// while (condition) {
+//     // code block to be executed
+//   }
+
+// let text = "";
+// let i = 0;
+
+// while (i < 10) {
+//     text += "The number is " + i;
+//     i++;
+//   }
+
+//   document.getElementById("demo").innerHTML = text;
+
+
+
+// let seconds = 10;
+
+// console.log("countdown");;
+
+// while (seconds > 0 ){
+//     console.log(seconds);
+//     seconds--
+// }
+
+// console.log("Happy Pongal Yo");
+
+
+// do {
+//     // Code to be executed
+// } while (condition);
+
+
+
+
+
+
+
+// let correctPassword = "12345";
+// let userPassword;
+// let attempts = 0;
+
+// do {
+//     userPassword = prompt("Enter your password:");
+//     attempts++;
+//     if (userPassword !== correctPassword) {
+//         console.log("Incorrect password, try again.");
+//     }
+// } while (userPassword !== correctPassword);
+
+// console.log(`Logged in successfully after ${attempts} attempts.`);
+
+
+
+// let num = 0 ;
+
+// do{
+//     console.log(num);
+//     num++;
+
+// }while (num < 0 );
+// console.log(num);
+
+
+// for (key in object){
+//     //code exicute 
+// }
+// const person = { 
+//     name: "Alice", 
+//     age: 25, 
+//     city: "New York"
+// };
+
+// const userProfile = {
+//     name: "Alice",
+//     age: 25,
+//     email: "alice@example.com",
+//     city: "New York"
+// };
+
+// console.log("User Profile:");
+// for (let key in userProfile) {
+//     console.log(`${key}: ${userProfile[key]}`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+// for (let key in person){
+//     console.log(`${key}:${person[key]}`);
+// }
+
+
+// for (Element if iteratables ){
+//     //code exicuted 
+// }
+
+// const colors = ["red", "green", "blue"];
+
+
+// for (let color of colors){
+//     console.log(color);
+// }
+
+
+// const str = "hello";
+
+// for (let char of str){
+//     console.log(char);
+
+// }
